b9a2ab4e4a61ef20d2fb5aba1556d3a2a67ddd66
xxgitvc <msci.2323@unigoa.ac.in>
2026-04-05 18:12:01 +0000

    Update REDIRECT_URI for Render deployment

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ NGROK = "https://unblamable-untempting-daniele.ngrok-free.dev"
 GOOGLE_AUTH_URL = "https://accounts.google.com/o/oauth2/v2/auth"
 GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token"
 GOOGLE_USER_URL = "https://www.googleapis.com/oauth2/v2/userinfo"
-REDIRECT_URI = "https://consoles-ctrl-demo-monroe.trycloudflare.com/login/callback"
+REDIRECT_URI = "https://bloodbank-project-b3zs.onrender.com/login/callback"
 
 def get_db():
     return mysql.connector.connect(
