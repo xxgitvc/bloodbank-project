efbd6a1de5db064f7e8fe3ad091e18cf9dca56b3
Jay Namdev Gaonkar <139752572+xxgitvc@users.noreply.github.com>
2026-04-10 21:37:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 import os
- 
-MYSQL_SOCKET   = "/cloudsql/blood-bank-project-492616:us-central1:bloodbank-db"
+
+MYSQL_HOST     = "34.47.228.232"
 MYSQL_USER     = os.environ.get("MYSQL_USER")
 MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD")
 MYSQL_DATABASE = os.environ.get("MYSQL_DATABASE")
- 
+
 SECRET_KEY = os.environ.get("SECRET_KEY")
- 
+
 DEBUG = True
- 
+
 GOOGLE_CLIENT_ID     = os.environ.get("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
- 
-PROJECT_ID   = "blood-bank-project-492616"
+
+PROJECT_ID   = "bloodbank-project-576134041271"
 MAPS_API_KEY = os.environ.get("MAPS_API_KEY")
- 
+
 BASE_URL = os.environ.get("BASE_URL")
